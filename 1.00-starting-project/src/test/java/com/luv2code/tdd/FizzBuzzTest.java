3ae7a83131376f11b8d59bdc2d906a9771c512b0
Michael Angelo Valente <miky10.mv@gmail.com>
2023-05-26 15:41:06 +0200

    Parametrized test - part1

--- 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -1,6 +1,8 @@
 package com.luv2code.tdd;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -46,5 +48,15 @@ class FizzBuzzTest {
         assertEquals(expected, FizzBuzz.compute(1), "Should return FizzBuzz");
     }
 
+
+    @DisplayName("Testing with small data file")
+    @ParameterizedTest(name="value={0}, expected={1}")
+    @CsvFileSource(resources ="/small-test-data.csv")
+    @Order(5)
+    void testSmallDataFile(int value, String expected){
+
+        assertEquals(expected, FizzBuzz.compute(value));
+
+    }
 
 }
