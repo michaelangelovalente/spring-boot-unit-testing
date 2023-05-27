dfcd07a09e1d667f0ff0234781b4951387544f75
Michael Angelo Valente <miky10.mv@gmail.com>
2023-05-27 23:24:40 +0200

    Created Mock. Injected Mock into Service

---
+++ 2.20-starting-project/src/test/java/com/luv2code/test/MockAnnotationTest.java
@@ -0,0 +1,39 @@
+package com.luv2code.test;
+
+import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.dao.ApplicationDao;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import com.luv2code.component.service.ApplicationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest(classes= MvcTestingExampleApplication.class)
+public class MockAnnotationTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    CollegeStudent studentOne;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+    @Mock
+    private ApplicationDao applicationDao; //test double
+
+    private ApplicationService applicationService;
+
+
+    @BeforeEach
+    public void beforeEach(){
+        studentOne.setFirstname("Eric");
+        studentOne.setLastname("Roby");
+        studentOne.setEmailAddress("eric.roby@gmail.com");
+        studentOne.setStudentGrades(studentGrades);
+    }
+}
