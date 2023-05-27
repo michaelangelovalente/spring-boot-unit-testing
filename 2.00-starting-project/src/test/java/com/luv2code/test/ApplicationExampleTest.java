f8e6e75c1528d0da0233b3170bd1aa4c3e09109b
Michael Angelo Valente <miky10.mv@gmail.com>
2023-05-27 17:13:47 +0200

    AssertEquals and AssertNotEquals

--- 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
+++ 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -1,13 +1,70 @@
 package com.luv2code.test;
 
 import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
 public class ApplicationExampleTest {
-    @Test
-    void basicTest(){
 
+    private static int count = 0;
+
+    @Value("${info.app.name}")
+    private String appInfo;
+
+    @Value("${info.app.description}")
+    private String appDescription;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+    @Value("${info.school.name}")
+    private String schoolName;
+
+    @Autowired
+    CollegeStudent student;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+
+    @BeforeEach
+    public void beforeEach(){
+        count = count + 1;
+        System.out.println("Testing: " + appInfo + " which is " + appDescription
+                + " Version: " + appVersion + ". Execution of test method " + count);
+        student.setFirstname("Eric");
+        student.setLastname("Roby");
+        student.setEmailAddress("eric.roby@gmail.com");
+        studentGrades.setMathGradeResults(new ArrayList<>(Arrays.asList( 100.0, 85.0, 76.50, 91.75)));
+        student.setStudentGrades(studentGrades);
+    }
+
+    @DisplayName("Add grade results for student grades")
+    @Test
+    public void addGradeResultsForStudentGrades(){
+        assertEquals( 353.25, studentGrades.addGradeResultsForSingleClass(
+                student.getStudentGrades().getMathGradeResults()
+        ));
+    }
+
+    @DisplayName("Add grade results for student grades not equal")
+    @Test
+    public void addGradeResultsForStudentGradesAssertNotEquals(){
+        assertNotEquals(0, studentGrades.addGradeResultsForSingleClass(
+                student.getStudentGrades().getMathGradeResults()
+        ));
     }
 }
