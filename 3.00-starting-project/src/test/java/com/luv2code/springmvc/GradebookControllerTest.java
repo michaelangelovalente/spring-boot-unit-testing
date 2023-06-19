aa6de87b86577d5a04b80f67d9f29e52e5ce4c5f
michaelangelovalente <miky10.mv@gmail.com>
2023-06-19 10:28:15 +0200

    Basic infrastructure - part2

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -1,8 +1,11 @@
 package com.luv2code.springmvc;
 
+import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.models.GradebookCollegeStudent;
 import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.batch.BatchProperties;
@@ -11,6 +14,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.Mockito.when;
 
 @TestPropertySource("/application.properties")
 @AutoConfigureMockMvc
@@ -24,12 +34,24 @@ public class GradebookControllerTest {
     private MockMvc mockMvc;
 
     @Mock
-    private StudentAndGradeService studentAndGradeService;
+    private StudentAndGradeService studentAndGradeServiceMock;
 
     @BeforeEach
     public void beforeEach(){
         jdbc.execute("INSERT INTO student(id, firstname, lastname, email_address)" +
                 "values(1, 'Eric', 'Roby', ' eric@gmail.com');");
+    }
+
+    @Test
+    public void getSTudentHttpRequest(){
+        CollegeStudent studentOne = new GradebookCollegeStudent("Eric", "Roby", "eric@gmail.com" );
+        CollegeStudent studentTwo = new GradebookCollegeStudent("Chad", "Darby", "chad@gmail.com");
+
+        List<CollegeStudent> collegeStudentList = new ArrayList<>(Arrays.asList(studentOne, studentTwo));
+
+        when(studentAndGradeServiceMock.getGradebook()).thenReturn(collegeStudentList);
+
+        assertIterableEquals(collegeStudentList, studentAndGradeServiceMock.getGradebook());
     }
 
     @AfterEach
