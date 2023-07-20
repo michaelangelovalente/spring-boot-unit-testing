850e15654153e01b0fed321f47ec4b7eb70c0c33
michaelangelovalente <miky10.mv@gmail.com>
2023-07-20 12:06:34 +0200

    TDD example: Create a grade for invalid student id

--- 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -246,6 +246,20 @@ public class GradebookControllerTest {
                     .andExpect(jsonPath("$.studentGrades.mathGradeResults", hasSize(2)));
 
     }
+
+    @Test
+    void createAValidGradeHttpRequestStudentDoesNotExistEmptyResponse() throws Exception {
+        this.mockMvc.perform(post("/grades")
+                .contentType(APPLICATION_JSON_UTF8)
+                .param("grade", "85.00")
+                .param("gradeType", "math")
+                .param("studentId", "0"))
+                    .andExpect(status().is4xxClientError())
+                    .andExpect(jsonPath("$.status", is(404)))
+                    .andExpect(jsonPath("$.message", is("Student or Grade was not found")));
+
+    }
+
     @AfterEach
     void setupAfterTransaction(){
         jdbc.execute(sqlDeleteStudent);
